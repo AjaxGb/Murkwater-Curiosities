94e6aa72e710d38f881ad3fde49f23ec647a779b
AjaxGb <ajaxgb@gmail.com>
2019-02-04 18:49:22 -0500

    Initial commit

---
+++ Assets/Scripts/SubController.cs
@@ -0,0 +1,43 @@
+﻿using Cinemachine;
+using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody2D))]
+public class SubController : MonoBehaviour {
+	
+	[SerializeField] private Rigidbody2D rb;
+	[SerializeField] private CinemachineTargetGroup targetGroup;
+
+	[SerializeField] private float riseSpeed = 0.3f;
+	[SerializeField] private float diveSpeed = 0.9f;
+	[SerializeField] private float sinkSpeed = 0.1f;
+	[SerializeField] private float horizSpeed = 0.6f;
+
+	private Vector2 velocity;
+
+	void Start() {
+		
+	}
+
+	void Update() {
+	}
+	
+	void FixedUpdate() {
+		velocity = Vector2.zero;
+
+		float vertInput = Input.GetAxis("Vertical");
+		float horizInput = Input.GetAxis("Horizontal");
+
+		if (vertInput < 0) {
+			velocity.y = vertInput * diveSpeed;
+		} else if (vertInput > 0) {
+			velocity.y = vertInput * riseSpeed;
+		} else {
+			velocity.y = -sinkSpeed;
+		}
+
+		velocity.x = horizInput * horizSpeed;
+
+		Debug.Log(velocity);
+		rb.MovePosition((Vector2)transform.position + velocity * Time.fixedDeltaTime);
+	}
+}
---
+++ README.md
@@ -0,0 +1,7 @@
+# Murkwater Curiosities
+
+RPI Experimental Game Design, Spring 2019 Second Project
+
+* Sol Toder - Programming
+* Chloe Fisher - Character, background, and UI art
+* Lewis - Writing and concept sketches
