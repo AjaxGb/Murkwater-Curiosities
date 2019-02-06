4dbc453fbe15e23dea6bc2dbd137a6f72c8191e5
AjaxGb <ajaxgb@gmail.com>
2019-02-06 18:08:40 -0500

    Bouyancy and surfacing works

--- Assets/Scripts/SubController.cs
+++ Assets/Scripts/SubController.cs
@@ -12,6 +12,16 @@ public class SubController : MonoBehaviour {
 	[SerializeField] private SpriteRenderer mainSprite;
 	[SerializeField] private CinemachineTargetGroup targetGroup;
 	[Space]
+	public float waterLevel = 0.5f;
+	[SerializeField] private float floatMidpoint = 0.5f;
+	[SerializeField] private float floatRadius = 1.5f;
+	[SerializeField] private float bouyancyRadiusBonus = 1.0f;
+	[SerializeField] private float maxBouyancy = 1.5f;
+	[SerializeField] private float outOfWaterGravity = 9.8f;
+	[SerializeField] private AnimationCurve gravityStrengthCurve;
+	[SerializeField] private AnimationCurve steeringStrengthCurve;
+	[SerializeField] private AnimationCurve bouyancyStrengthCurve;
+	[Space]
 	[SerializeField] private float riseSpeed = 0.3f;
 	[SerializeField] private float riseAccel = 1.0f;
 	[SerializeField] private float diveSpeed = 0.9f;
@@ -31,11 +41,12 @@ public class SubController : MonoBehaviour {
 	public VState VerticalState { get; private set; }
 	public HState HorizontalState { get; private set; }
 
+	private Vector2 lastPosition;
 	private Vector2 velocity;
 	private float rotVelocity;
 
 	void Start() {
-
+		lastPosition = rb.position;
 	}
 
 	void Update() {
@@ -43,8 +54,38 @@ public class SubController : MonoBehaviour {
 	
 	void FixedUpdate() {
 
+		Vector2 currPosition = rb.position;
+		Vector2 realVelocity = (currPosition - lastPosition) / Time.fixedDeltaTime;
+		Utils.ClampSymmetric(ref velocity.x, Mathf.Abs(realVelocity.x * 1.1f));
+		Utils.ClampSymmetric(ref velocity.y, Mathf.Abs(realVelocity.y * 1.1f));
+
 		Vector2 targetVel = Vector2.zero;
 		float targetRot = 0.0f;
+
+		float inWaterPercent;
+		float outOfWaterPercent;
+		float atSurfacePercent;
+		{
+			float floatMidpoint = currPosition.y + this.floatMidpoint;
+			float floatBottom = floatMidpoint - floatRadius;
+			float floatTop = floatMidpoint + floatRadius;
+			inWaterPercent = Mathf.InverseLerp(floatBottom, floatTop, waterLevel);
+			outOfWaterPercent = 1 - inWaterPercent;
+
+			if (inWaterPercent < 0.5f) {
+				atSurfacePercent = 1 - (0.5f - inWaterPercent) * 2;
+			} else {
+				atSurfacePercent = Mathf.InverseLerp(floatTop + bouyancyRadiusBonus, floatMidpoint, waterLevel);
+			}
+
+#if UNITY_EDITOR
+			Debug.LogFormat("In Water: {0:P}\nAt Surface: {1:P}", inWaterPercent, atSurfacePercent);
+			Debug.DrawRay(new Vector2(currPosition.x - 2, waterLevel), Vector2.right * 4, Color.blue);
+			Debug.DrawLine(new Vector2(currPosition.x, floatBottom), new Vector2(currPosition.x, floatTop), Color.red);
+			Debug.DrawRay(new Vector2(currPosition.x, floatTop), Vector2.up * bouyancyRadiusBonus, Color.green);
+			Debug.DrawRay(new Vector2(currPosition.x - 0.1f, floatMidpoint), Vector2.right * 0.2f, Color.red);
+#endif
+		}
 
 		float xMaxAccel, yMaxAccel;
 
@@ -88,14 +129,23 @@ public class SubController : MonoBehaviour {
 		Vector2 acceleration = targetVel - velocity;
 		Utils.ClampSymmetric(ref acceleration.x, xMaxAccel * Time.fixedDeltaTime);
 		Utils.ClampSymmetric(ref acceleration.y, yMaxAccel * Time.fixedDeltaTime);
+		velocity += acceleration * steeringStrengthCurve.Evaluate(inWaterPercent);
 
-		velocity += acceleration;
-		
+		if (VerticalState != VState.DIVE) {
+			Vector2 bouyancy = Vector2.up * maxBouyancy * Time.fixedDeltaTime;
+			velocity += bouyancy * bouyancyStrengthCurve.Evaluate(atSurfacePercent);
+		}
+
+		Vector2 gravity = Vector2.down * outOfWaterGravity * Time.fixedDeltaTime;
+		velocity += gravity * gravityStrengthCurve.Evaluate(outOfWaterPercent);
+
 		rb.MovePosition(rb.position + velocity * Time.fixedDeltaTime);
 
 		// Rotation
 		
 		rb.MoveRotation(
 			Mathf.SmoothDampAngle(rb.rotation, targetRot, ref rotVelocity, rightingTime, rightingSpeed));
+
+		lastPosition = currPosition;
 	}
 }
